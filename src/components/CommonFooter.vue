3c5f7c6bcce4d76d13f17346cb9a7cd5e9abbcb8
zhaoxuefei <1120906264@qq.com>
2021-01-27 15:34:17 +0800

    实现CommonFooter，部分主页

--- src/components/CommonFooter.vue
+++ src/components/CommonFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="common-footer">
-    <a-row>
+    <a-row class="top-show-box">
       <a-col :span="24">
         <div class="top-box">
           <span href="#"><img :src="GFSQ" alt="" />官方授权</span>
@@ -11,6 +11,44 @@
           <a-divider type="vertical" />
           <span href="#"><img :src="SHBZ" alt="" />售后保障</span>
         </div>
+      </a-col>
+    </a-row>
+    <a-row class="bottom-show-box">
+      <a-col :span="13" class="bottom-show-left">
+        <a-row>
+          <a-col :span="6">
+            <ul>
+              <li v-for="(item, idx) in servePro" :key="idx">{{ item }}</li>
+            </ul>
+          </a-col>
+          <a-col :span="6">
+            <ul>
+              <li v-for="(pay, idx) in payWay" :key="idx">{{ pay }}</li>
+            </ul>
+          </a-col>
+          <a-col :span="6"
+            ><ul>
+              <li v-for="(bSer, idx) in businessServe" :key="idx">
+                {{ bSer }}
+              </li>
+            </ul></a-col
+          >
+          <a-col :span="6"
+            ><ul>
+              <li v-for="(wlps, idx) in logisticsDistribution" :key="idx">
+                {{ wlps }}
+              </li>
+            </ul></a-col
+          >
+        </a-row>
+      </a-col>
+      <a-col :span="5" class="bottom-show-right">
+        <ul>
+          <li class="telTitle">{{ footerMessage.telTitle }}</li>
+          <li class="telNum">{{ footerMessage.telNum }}</li>
+          <li class="telTime">{{ footerMessage.telTime }}</li>
+          <li class="telPeople">{{ footerMessage.telPeople }}</li>
+        </ul>
       </a-col>
     </a-row>
   </div>
@@ -28,10 +66,108 @@ export default {
       ZPBZ,
       QTWLY,
       SHBZ,
+      servePro: [
+        "服务保障",
+        "正品保证",
+        "七天无理由退换",
+        "退货返运费",
+        "7X15小时客服服务",
+      ],
+      payWay: ["支付方式", "公司转账", "货到付款", "在线支付", "分期付款"],
+      businessServe: [
+        "商家服务",
+        "商家服务",
+        "培训中心",
+        "广告服务",
+        "服务市场",
+      ],
+      logisticsDistribution: [
+        "物流配送",
+        "免运费",
+        "海外配送",
+        "EMS",
+        "211限时达",
+      ],
+      footerMessage: {
+        telTitle: "全国客服热线",
+        telNum: "000-0000-000",
+        telTime: "上午 8:00-12：00    下午 13：00-19：00",
+        telPeople: "客服中心",
+      },
     };
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.common-footer {
+  // width: 100%;
+  // position: fixed;
+  // left: 0;
+  // bottom: 0;
+  // z-index: 999;
+  padding: 40px 20px;
+  background-color: #333333;
+
+  .top-show-box {
+    height: 100px;
+    line-height: 100px;
+
+    .top-box span {
+      display: inline-block;
+      width: 20%;
+      font-size: 16px;
+      vertical-align: middle;
+      color: rgb(183, 183, 183);
+    }
+
+    img {
+      vertical-align: middle;
+      margin-right: 20px;
+    }
+  }
+
+  .bottom-show-box {
+    margin-top: 35px;
+
+    .bottom-show-left {
+      margin-left: 195px;
+
+      li {
+        text-align: left;
+        font-size: 12px;
+        color: rgb(102, 102, 102);
+        height: 40px;
+        line-height: 40px;
+
+        &:first-child {
+          font-size: 16px;
+          color: rgb(183, 183, 183);
+        }
+      }
+    }
+
+    .bottom-show-right {
+      padding-top: 20px;
+      float: right;
+      margin-right: 100px;
+      li {
+        height: 30px;
+        line-height: 30px;
+        color: #999;
+        text-align: right;
+      }
+
+      .telTitle {
+        font-size: 16px;
+        font-weight: bold;
+      }
+
+      .telNum {
+        color: #999;
+        font-weight: bold;
+      }
+    }
+  }
+}
 </style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,7 +2,38 @@
   <div class="index">
     <common-header></common-header>
     <a-row>
-      <a-col :span="24"> col-24 </a-col>
+      <a-col :span="24">
+        <div class="nav-container">
+          <div class="home">宠物用品</div>
+          <div class="nav-box">
+            <a-menu v-model="current" mode="horizontal">
+              <a-menu-item key="mailOne">
+                <a-icon type="mail" />首页
+              </a-menu-item>
+              <a-menu-item key="mailTwo">
+                <a-icon type="mail" />口粮推荐</a-menu-item
+              ><a-menu-item key="mailThree">
+                <a-icon type="mail" />新品宝贝</a-menu-item
+              ><a-menu-item key="mailFour">
+                <a-icon type="mail" />全部商品
+              </a-menu-item>
+            </a-menu>
+          </div>
+          <div class="shop-box">
+            <a href="#"
+              ><a-icon
+                :style="{
+                  fontSize: '20px',
+                  marginRight: '8px',
+                  color: '#6091e2',
+                }"
+                type="shopping-cart"
+              />购物车</a
+            >
+          </div>
+        </div>
+        <div class="content-box"></div>
+      </a-col>
     </a-row>
     <common-footer></common-footer>
   </div>
@@ -14,7 +45,9 @@ import CommonFooter from "../components/CommonFooter.vue";
 export default {
   components: { CommonHeader, CommonFooter },
   data() {
-    return {};
+    return {
+      current: ["mailOne"],
+    };
   },
 };
 </script>
@@ -22,8 +55,54 @@ export default {
 <style lang="scss" scoped>
 .index {
   margin-top: 36px;
-  .ant-col {
-    background-color: #ccc;
+  margin-bottom: 415px;
+  // .ant-col {
+  // background-color: #ccc;
+  // }
+
+  .nav-container {
+    position: relative;
+    width: 1200px;
+    margin: 0 auto;
+    height: 100px;
+    line-height: 100px;
+    background-color: #fff;
+
+    & > div {
+      float: left;
+    }
+
+    .home {
+      font-size: 22px;
+      color: rgb(25, 114, 209);
+      font-weight: bold;
+    }
+
+    .nav-box {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+
+    .shop-box {
+      float: right;
+
+      a {
+        display: inline-block;
+        border: 1px solid #e6e6e6;
+        background-color: #fff;
+        height: 42px;
+        line-height: 42px;
+        padding: 0 40px 0 54px;
+        color: #919191;
+        font-size: 12px;
+      }
+    }
+  }
+  .content-box {
+    height: 2000px;
+    background: url("../assets/images/bg.jpg") repeat top, transparent;
   }
 }
 </style>
