ed7b31c05eee0dd72179b5c18aec98351d3a95cc
Zxf21 <zhao.xuefei21@outlook.com>
2021-01-27 17:48:19 +0800

    首页添加回到顶部按钮

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -29,6 +29,14 @@
       </div> -->
     </div>
     <common-footer></common-footer>
+    <div id="components-back-top-demo-custom">
+      <a-back-top>
+        <div class="ant-back-top-inner">UP</div>
+      </a-back-top>
+      Scroll down to see the bottom-right
+      <strong style="color: #1088e9"> blue </strong>
+      button.
+    </div>
   </div>
 </template>
 
@@ -114,6 +122,20 @@ export default {
     // .dayily-show {
     //   font-size: 20px;
     // }
+  }
+
+  #components-back-top-demo-custom .ant-back-top {
+    bottom: 100px;
+  }
+  #components-back-top-demo-custom .ant-back-top-inner {
+    height: 40px;
+    width: 40px;
+    line-height: 40px;
+    border-radius: 4px;
+    background-color: #1088e9;
+    color: #fff;
+    text-align: center;
+    font-size: 20px;
   }
 }
 </style>
