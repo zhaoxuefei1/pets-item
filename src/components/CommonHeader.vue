0cbee0dedfbe25207175c70e63e1c6f29a43ced7
zhaoxuefei <1120906264@qq.com>
2021-01-27 17:34:27 +0800

    继续实现index页面

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="common-header">
-    <a-row>
+    <a-row class="top-nav">
       <a-col :span="12">
         <div class="left-box">
           <a href="#">首页</a>
@@ -28,28 +28,109 @@
         </div>
       </a-col>
     </a-row>
+    <a-row class="bottom-nav">
+      <a-col :span="24">
+        <div class="nav-container">
+          <div class="home">宠物用品</div>
+          <div class="nav-box">
+            <a-menu v-model="current" mode="horizontal">
+              <a-menu-item class="nav" key="mailOne"> 首页 </a-menu-item>
+              <a-menu-item class="nav" key="mailTwo"> 口粮推荐</a-menu-item
+              ><a-menu-item class="nav" key="mailThree"> 新品宝贝</a-menu-item
+              ><a-menu-item class="nav" key="mailFour"> 全部商品 </a-menu-item>
+            </a-menu>
+          </div>
+          <div class="shop-box">
+            <a href="#"
+              ><a-icon
+                :style="{
+                  fontSize: '20px',
+                  marginRight: '8px',
+                  color: '#6091e2',
+                }"
+                type="shopping-cart"
+              />购物车</a
+            >
+          </div>
+        </div>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      current: ["mailOne"],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .common-header {
-  width: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  background-color: #f5f5f5;
-  z-index: 9999;
-  .ant-row {
+  background-color: #fff;
+  .top-nav {
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
     height: 36px;
     line-height: 36px;
+    background-color: #f5f5f5;
+    z-index: 9999;
 
     a {
       font-size: 12px;
       color: #666;
+    }
+  }
+  .bottom-nav {
+    .nav-container {
+      position: relative;
+      width: 1200px;
+      margin: 0 auto;
+      height: 100px;
+      line-height: 100px;
+      background-color: #fff;
+
+      & > div {
+        float: left;
+      }
+
+      .home {
+        font-size: 22px;
+        color: rgb(25, 114, 209);
+        font-weight: bold;
+      }
+
+      .nav-box {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+
+        .nav {
+          font-size: 16px;
+          color: #4d4d4d;
+        }
+      }
+
+      .shop-box {
+        float: right;
+
+        a {
+          display: inline-block;
+          border: 1px solid #e6e6e6;
+          background-color: #fff;
+          height: 42px;
+          line-height: 42px;
+          padding: 0 40px 0 54px;
+          color: #919191;
+          font-size: 12px;
+        }
+      }
     }
   }
 }
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,40 +1,33 @@
 <template>
   <div class="index">
     <common-header></common-header>
-    <a-row>
-      <a-col :span="24">
-        <div class="nav-container">
-          <div class="home">宠物用品</div>
-          <div class="nav-box">
-            <a-menu v-model="current" mode="horizontal">
-              <a-menu-item key="mailOne">
-                <a-icon type="mail" />首页
-              </a-menu-item>
-              <a-menu-item key="mailTwo">
-                <a-icon type="mail" />口粮推荐</a-menu-item
-              ><a-menu-item key="mailThree">
-                <a-icon type="mail" />新品宝贝</a-menu-item
-              ><a-menu-item key="mailFour">
-                <a-icon type="mail" />全部商品
-              </a-menu-item>
-            </a-menu>
-          </div>
-          <div class="shop-box">
-            <a href="#"
-              ><a-icon
-                :style="{
-                  fontSize: '20px',
-                  marginRight: '8px',
-                  color: '#6091e2',
-                }"
-                type="shopping-cart"
-              />购物车</a
-            >
-          </div>
-        </div>
-        <div class="content-box"></div>
-      </a-col>
-    </a-row>
+    <div class="content-box">
+      <div class="get-coupon">
+        <a-row>
+          <a-col
+            class="coupon"
+            :span="8"
+            v-for="(payMessage, idx) in getCouponObj"
+            :key="idx"
+          >
+            <img :src="getCouponBg" alt="" />
+            <div class="pay-message">
+              <p class="top-tips">
+                <span class="way">{{ payMessage.way }}</span>
+                <span class="pay-num">{{ payMessage.payNum }}</span>
+              </p>
+              <p class="pay-tips">{{ payMessage.payTips }}</p>
+            </div>
+          </a-col>
+        </a-row>
+      </div>
+      <!-- <div class="dayily-show">
+        <h2></h2>
+        <ul>
+          <li></li>
+        </ul>
+      </div> -->
+    </div>
     <common-footer></common-footer>
   </div>
 </template>
@@ -42,11 +35,32 @@
 <script>
 import CommonHeader from "../components/CommonHeader.vue";
 import CommonFooter from "../components/CommonFooter.vue";
+import getCouponBg from "../assets/images/bgImg.webp";
+import middleBg from "../assets/images/middleBg.webp";
 export default {
   components: { CommonHeader, CommonFooter },
   data() {
     return {
+      getCouponBg,
+      middleBg,
       current: ["mailOne"],
+      getCouponObj: [
+        {
+          way: "¥",
+          payNum: 20,
+          payTips: "满199元可用",
+        },
+        {
+          way: "¥",
+          payNum: 30,
+          payTips: "满269元可用",
+        },
+        {
+          way: "¥",
+          payNum: 50,
+          payTips: "满389元可用",
+        },
+      ],
     };
   },
 };
@@ -56,53 +70,50 @@ export default {
 .index {
   margin-top: 36px;
   margin-bottom: 415px;
-  // .ant-col {
-  // background-color: #ccc;
-  // }
-
-  .nav-container {
-    position: relative;
-    width: 1200px;
-    margin: 0 auto;
-    height: 100px;
-    line-height: 100px;
-    background-color: #fff;
-
-    & > div {
-      float: left;
-    }
-
-    .home {
-      font-size: 22px;
-      color: rgb(25, 114, 209);
-      font-weight: bold;
-    }
-
-    .nav-box {
-      position: absolute;
-      left: 50%;
-      top: 50%;
-      transform: translate(-50%, -50%);
-    }
-
-    .shop-box {
-      float: right;
-
-      a {
-        display: inline-block;
-        border: 1px solid #e6e6e6;
-        background-color: #fff;
-        height: 42px;
-        line-height: 42px;
-        padding: 0 40px 0 54px;
-        color: #919191;
-        font-size: 12px;
-      }
-    }
-  }
   .content-box {
     height: 2000px;
     background: url("../assets/images/bg.jpg") repeat top, transparent;
+
+    .get-coupon {
+      padding-top: 850px;
+
+      .coupon {
+        position: relative;
+
+        .pay-message {
+          position: absolute;
+          top: 5%;
+          left: 50%;
+          transform: translateX(-50%);
+
+          p {
+            color: #9dd0f7;
+            font-weight: bold;
+          }
+
+          .top-tips {
+            margin-top: -10px;
+          }
+
+          .way {
+            font-size: 43px;
+          }
+
+          .pay-num {
+            font-size: 98px;
+          }
+
+          .pay-tips {
+            font-size: 30px;
+            margin-top: -50px;
+          }
+        }
+      }
+    }
+
+    // .dayily-show {
+    //   font-size: 20px;
+    // }
   }
 }
 </style>
