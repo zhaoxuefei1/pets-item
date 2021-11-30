8d131bf2b2d4d75919421231acf28cb6f6a2f109
赵雪飞 <9178690+zhao-xuefei@user.noreply.gitee.com>
2021-11-30 14:37:20 +0800

    test

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -34,7 +34,7 @@
         <div class="ant-back-top-inner">UP</div>
       </a-back-top>
       Scroll down to see the bottom-right
-      <strong style="color: #1088e9"> blue </strong>
+      <strong style="color: #1088e9"> red </strong>
       button.
     </div>
   </div>
